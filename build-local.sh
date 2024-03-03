d9d608ee7d95465bb216de8759628170222821b3
Tom Coombs <tom@cube74.com>
2024-03-03 13:23:25 -0500

    converted to ZMA

--- build-local.sh
+++ build-local.sh
@@ -1,7 +1,7 @@
 ﻿Write-Output "Building executable"
 dotnet publish -c Release
 Write-Output "Building site"
-.\bin\Release\net7.0\publish\charts-site-generator.exe .\_public\
+.\bin\Release\net7.0\charts-site-generator.exe .\_public\
 npx tailwindcss -i .\style.css -o .\_public\style.css
 Copy-Item script.js .\_public\ -Force
 Copy-Item favicon.ico .\_public\ -Force
